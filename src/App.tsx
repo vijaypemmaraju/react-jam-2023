bcc9ee811884cfc04f2d6e55eb1870955e41a22f
Vijay Pemmaraju <vijay@grooveapp.com>
2023-07-20 16:24:44 -0700

    Move to cursor via bezier curve

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Bezier, Point } from "bezier-js";
+
 import { Sprite, useTick, useApp } from "@pixi/react";
 import { useEffect, useState } from "react";
 import "./App.css";
@@ -6,8 +8,7 @@ import useStore from "./useStore";
 function App() {
   // const blurFilter = useMemo(() => new BlurFilter(4), []);
   const [position, setPosition] = useState({ x: 400, y: 270 });
-  const destination = useStore((state) => state.destination);
-  const setDestination = useStore((state) => state.setDestination);
+  const [curve, setCurve] = useState<Point[]>();
   const app = useApp();
 
   useEffect(() => {
@@ -16,22 +17,46 @@ function App() {
       const rect = root?.getBoundingClientRect();
       const x = e.clientX - (rect?.left || 0);
       const y = e.clientY - (rect?.top || 0);
-      setDestination(x, y);
+      const length = Math.sqrt(
+        Math.pow(position.x - x, 2) + Math.pow(position.y - y, 2)
+      );
+      const curveAmount = length / 4;
+      const bezier =
+        new Bezier([
+          { x: position.x, y: position.y },
+          // choose a random point near the midpoint of the line
+          {
+            x:
+              (position.x + x) / 2 +
+              Math.random() * curveAmount -
+              curveAmount / 2,
+            y:
+              (position.y + y) / 2 +
+              Math.random() * curveAmount -
+              curveAmount / 2,
+          },
+          { x: x, y: y },
+        ])
+      setCurve(bezier.getLUT(length * 0.2));
     };
     document.addEventListener("mousedown", listener);
 
     return () => {
       document.removeEventListener("mousedown", listener);
     };
-  }, [app, setDestination]);
+  }, [app, position.x, position.y]);
 
-  useTick(() => {
-    // console.log(app.renderer.plugins.interaction.pointer);
-    if (destination) {
-      setPosition((position) => ({
-        x: position.x + (destination.x - position.x) * 0.1,
-        y: position.y + (destination.y - position.y) * 0.1,
-      }));
+  useTick((_delta) => {
+    if (curve) {
+      if (curve.length === 0) {
+        setCurve(undefined);
+        return;
+      }
+      const position = curve.shift();
+      setPosition({
+        x: position!.x,
+        y: position!.y,
+      });
     }
   });
 
