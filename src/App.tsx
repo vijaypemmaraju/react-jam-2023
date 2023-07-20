3a63a5720f91fb564ccdd9a858552ecbdd5a0a7c
Vijay Pemmaraju <vijay@grooveapp.com>
2023-07-20 12:15:36 -0700

    Clean up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { BlurFilter } from 'pixi.js';
-import { Stage, Container, Sprite, Text, useTick, useApp } from '@pixi/react';
-import { useEffect, useMemo, useState } from 'react';
-import './App.css'
-import useStore from './useStore';
+import { BlurFilter } from "pixi.js";
+import { Stage, Container, Sprite, Text, useTick, useApp } from "@pixi/react";
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import useStore from "./useStore";
 
 function App() {
-  const blurFilter = useMemo(() => new BlurFilter(4), []);
+  // const blurFilter = useMemo(() => new BlurFilter(4), []);
   const [position, setPosition] = useState({ x: 400, y: 270 });
-  const destination = useStore(state => state.destination);
-  const setDestination = useStore(state => state.setDestination);
+  const destination = useStore((state) => state.destination);
+  const setDestination = useStore((state) => state.setDestination);
   const app = useApp();
 
   useEffect(() => {
-    const root = document.querySelector('canvas');
+    const root = document.querySelector("canvas");
     const listener = (e) => {
       const rect = root?.getBoundingClientRect();
       const x = e.clientX - (rect?.left || 0);
       const y = e.clientY - (rect?.top || 0);
       setDestination(x, y);
-    }
-    document.addEventListener('mousedown', listener);
+    };
+    document.addEventListener("mousedown", listener);
 
     return () => {
-      document.removeEventListener('mousedown', listener);
-    }
-
+      document.removeEventListener("mousedown", listener);
+    };
   }, [app, setDestination]);
-
 
   useTick(() => {
     // console.log(app.renderer.plugins.interaction.pointer);
@@ -40,18 +38,14 @@ function App() {
 
   return (
     <>
-    <Sprite
+      <Sprite
         image="https://pixijs.io/pixi-react/img/bunny.png"
         x={position.x}
         y={position.y}
         anchor={{ x: 0.5, y: 0.5 }}
       />
-
-      <Container x={400} y={330}>
-        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} filters={[blurFilter]} />
-      </Container>
-      </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,17 @@
-import { BlurFilter } from 'pixi.js';
-import { Stage, Container, Sprite, Text, useTick } from '@pixi/react';
-import { useMemo, useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import useStore from './useStore';
+import { Stage } from "@pixi/react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-
-export const MyComponent = () =>
-{
+export const MyComponent = () => {
   return (
-    <Stage >
+    <Stage>
       <App />
     </Stage>
   );
 };
 
-const container = document.createElement('div');
-container.id = 'root';
+const container = document.createElement("div");
+container.id = "root";
 document.body.appendChild(container);
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
