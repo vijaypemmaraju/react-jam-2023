4880778d030a2342dea6a094ac1c8e281ac8dda7
Vijay Pemmaraju <vijay@grooveapp.com>
2023-07-26 22:35:36 -0700

    Make sounds work on mobile

--- src/sounds.tsx
+++ src/sounds.tsx
@@ -1,11 +1,11 @@
 import { sound, filters } from "@pixi/sound";
 
-sound.add("ambient", "sounds/ambient.ogg");
+sound.add("ambient", "sounds/ambient.mp3");
 sound.add("wing_flap", "sounds/wing_flap.wav");
 sound.add("wing_flap_2", "sounds/wing_flap_2.wav");
 sound.add("woosh", "sounds/woosh.flac");
-sound.add("fan_loop", "sounds/fan_loop.ogg");
-sound.add("song", "sounds/jaybae.ogg");
+sound.add("fan_loop", "sounds/fan_loop.mp3");
+sound.add("song", "sounds/jaybae.flac");
 
 export const fanLoopFilters = [
   new filters.TelephoneFilter(),
